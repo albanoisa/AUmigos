4489bb32a93e560383254275e1b468b2975faa07
Isabela_Albano <isabela_albano@estudante.sesisenai.org.br>
2026-08-24 11:12:22 -0300

    script pronto

--- database/script.db.sql
+++ database/script.db.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Synchronization
--- Generated: 2026-08-24 10:55
+-- Generated: 2026-08-24 11:11
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
@@ -22,24 +22,29 @@ ALTER TABLE `aumigos`.`cadastro_cliente`
 DROP FOREIGN KEY `fk_cadastro_cliente_id_clientes1`;
 
 ALTER TABLE `aumigos`.`id_clientes` 
-ADD INDEX `fk_id_clientes_id_animais_idx` (`id_animais_id_animais` ASC) VISIBLE,
-DROP INDEX `fk_id_clientes_id_animais_idx` ;
-;
-
-ALTER TABLE `aumigos`.`id_animais` 
-ADD INDEX `fk_id_animais_cadastro_animal1_idx` (`cadastro_animal_idcadastro_animal` ASC) VISIBLE,
-DROP INDEX `fk_id_animais_cadastro_animal1_idx` ;
-;
+ADD COLUMN `vizualizar_dados_clientes` VARCHAR(45) NOT NULL AFTER `id_clientescol`;
 
 ALTER TABLE `aumigos`.`cadastro_animal` 
-ADD INDEX `fk_cadastro_animal_cadastro_cliente1_idx` (`cadastro_cliente_idcadastro_cliente` ASC) VISIBLE,
-DROP INDEX `fk_cadastro_animal_cadastro_cliente1_idx` ;
-;
+ADD COLUMN `listar_animais` VARCHAR(45) NOT NULL AFTER `gerenciar_animal`;
 
 ALTER TABLE `aumigos`.`cadastro_cliente` 
-ADD INDEX `fk_cadastro_cliente_id_clientes1_idx` (`id_clientes_id_clientes` ASC) VISIBLE,
-DROP INDEX `fk_cadastro_cliente_id_clientes1_idx` ;
-;
+ADD COLUMN `listar_clientes` VARCHAR(45) NOT NULL AFTER `gerenciar_clientes`;
+
+CREATE TABLE IF NOT EXISTS `aumigos`.`SIstema_Clientes` (
+  `idSIstema_Clientes` INT(11) NOT NULL,
+  `SIstema_Clientescol` VARCHAR(45) NOT NULL,
+  `editar_cliente` VARCHAR(45) NOT NULL,
+  `excluir_cliente` VARCHAR(45) NOT NULL,
+  `id_clientes_id_clientes` INT(11) NOT NULL,
+  PRIMARY KEY (`idSIstema_Clientes`),
+  INDEX `fk_SIstema_Clientes_id_clientes1_idx` (`id_clientes_id_clientes` ASC) VISIBLE,
+  CONSTRAINT `fk_SIstema_Clientes_id_clientes1`
+    FOREIGN KEY (`id_clientes_id_clientes`)
+    REFERENCES `aumigos`.`id_clientes` (`id_clientes`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 ALTER TABLE `aumigos`.`id_clientes` 
 ADD CONSTRAINT `fk_id_clientes_id_animais`
