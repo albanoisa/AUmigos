c89e652000a8daa1e4ebf1ad9c6db356eaaf7aca
Isabela_Albano <isabela_albano@estudante.sesisenai.org.br>
2026-08-24 11:23:43 -0300

    index sendo feito

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de Pratos</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body> 
+    <main>
+        <h2>Pet Shop AUmigos</h2>
+        <nav>
+            <ul>
+                <li><a href="index.php">Cadastrar Clientes</a></li>
+                <li><a href="create.php">Listar Clientes</a></li>
+                <li><a href="read.php">Editar Clientes</a></li>
+                <li><a href="delete.php">Excluir Clientes</a></li>
+                <li><a href="create.php">Visualizar dados de Clientes</a></li>
+            </ul>
