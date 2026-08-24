71cdad97e1a1c81eb2dc1d0120727b3817f6486f
Isabela_Albano <isabela_albano@estudante.sesisenai.org.br>
2026-08-24 11:44:31 -0300

    index e css

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gerenciador de Pratos</title>
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="style/style.css">
 </head>
 
 <body> 
